18f9e1b1fb87f09c2dbd6e98a46b0e1913c0fed8
Kdheeru12 <60852451+Kdheeru12@users.noreply.github.com>
2020-05-20 18:46:38 +0530

    Add files via upload

---
+++ testing/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+class Orders(models.Model):
+    name = models.CharField(max_length=40)
+    phonenumber = models.CharField(max_length=10)
+    alt_number = models.CharField(max_length=10,blank=True)
+    address = models.CharField(max_length=800)
+    landmark = models.CharField(max_length=300)
+    ordered = models.FileField(upload_to='Orders/ordered')
+    ordered_date = models.DateTimeField(auto_now_add=True)    
+    def __str__(self):
+        return self.phonenumber
+
+ 
